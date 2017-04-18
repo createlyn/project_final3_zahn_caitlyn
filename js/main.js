63b7728153c63c7463d2fad45c816c2c2b074b23
Caitlyn Zahn <caitlyn.zahn@gmail.com>
2017-04-18 13:52:35 -0400

    added menu on click effect

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
-// $('#appetizers').animatescroll({
-//     swing: 400
-// });
+// smooth scroll animation
+$(document).on('click', 'a', function (event) {
+    event.preventDefault();
 
-$(document).ready(function () {
-    console.log('ready!');
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
 });
+
+
+function openMenu(evt, menuName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName('tabcontent');
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = 'none';
+    }
+    tablinks = document.getElementsByClassName('tablinks');
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace('active', '');
+    }
+    document.getElementById(menuName).style.display = 'block';
+    evt.currentTarget.className += 'active';
+}
