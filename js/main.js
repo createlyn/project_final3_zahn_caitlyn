6b651484e2fec0ad8cc39c943c76b5a0bed54fd3
Caitlyn Zahn <caitlyn.zahn@gmail.com>
2017-04-18 14:19:24 -0400

    added sticky feature to menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 // smooth scroll animation
 $(document).on('click', 'a', function (event) {
     event.preventDefault();
-
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 });
+
+// sticky container
+$('#menu').stickem();
 
 
 function openMenu(evt, menuName) {
